b26e300de26fe99e63dc172b6cb0135886910d86
Rodrigues42 <rodrigues.silva1998@outlook.com>
2021-10-01 10:24:33 -0300

    view inicial

---
+++ src/views/Inicio.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container>
+        <h1>Inicio safs</h1>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'Inicio'
+}
+</script>
+
+<style scoped>
+
+</style>
