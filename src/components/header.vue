c1e99bf6ba070a569764cec034638b61ed7dd8c2
RGonzalesR <renangonzales@gmail.com>
2021-10-01 10:23:04 -0300

    Barra inicial

---
+++ src/components/header.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-app-bar
+      app
+      shrink-on-scroll
+    >
+
+      <v-toolbar-title>Random Users</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
