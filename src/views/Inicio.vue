ed4f5eb2335c60e0ecb4610916d11687686a5fc8
Leonardo <leozikasantos73@gmail.com>
2021-10-01 10:47:25 -0300

    criando funcao de requisição para a view Inicio

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -11,6 +11,19 @@ export default {
     name: 'Inicio',
     components: {
         Card
+    },
+    data(){
+    return{
+      pessoasLista: []
+    }
+  },
+    created(){
+        fetch('https://randomuser.me/api/?page=1&results=25')
+        .then(response => response.json())
+        .then(json => {
+        this.pessoasLista = json.results
+        console.log(this.pessoasLista)
+        })
     }
 }
 </script>
@@ -18,3 +31,5 @@ export default {
 <style scoped>
 
 </style>
+
+
